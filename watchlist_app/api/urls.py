98ca82fdebdcdcde48df00f02bb854b6615f3a71
karankumar-07 <codered1704@gmail.com>
2024-03-11 18:09:17 +0530

    Refactored Code

---
+++ watchlist_app/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from watchlist_app.api.views import (WatchListAV,
+                                     WatchDetailAV, StreamPlatformAV,
+                                     StreamPlatformDetailAV)
+
+urlpatterns = [
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
+
+    path('stream/', StreamPlatformAV.as_view(), name='stream-list'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+
+]
